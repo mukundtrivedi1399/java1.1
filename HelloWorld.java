afdeeeba032b5c3a388c94748a790ed9b0431ff9
mukundtrivedi1399 <95852685+mukundtrivedi1399@users.noreply.github.com>
2023-01-07 18:30:35 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-public classby Mukund HelloWorld {
+public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
       System.out.println("Hello all, Shahataaz and Yogesh");
